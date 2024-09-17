a2588eb3df57fbe80694b3d777c39021c7e9c093
Taras Shyika <shyika_taras@ukr.net>
2024-09-17 12:03:10 -0500

    feat: added reac-router-dom and code spliting for pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
