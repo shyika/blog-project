155a4baf3a6d1da317fe2196c031ad09450b426b
Taras Shyika <shyika_taras@ukr.net>
2024-09-21 16:48:53 -0500

    refactor: added routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,9 @@
-import { Link, Route, Routes } from 'react-router-dom';
-
-import { Suspense } from 'react';
-
+import { Link } from 'react-router-dom';
 
 import './styles/index.scss'
 import { useTheme } from 'app/providers/ThemeProvider';
-import { classNames } from 'shared/lib/classNames';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from 'app/providers/router';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -17,12 +13,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to="/">Main page</Link>
             <Link to="/about">About page</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/" element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
