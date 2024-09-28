dcb27f9a577896d190684b262aff43591ac4018c
Taras Shyika <shyika_taras@ukr.net>
2024-09-28 12:47:44 -0500

    test(initial): added jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,20 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with 3rd param', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2']))
+            .toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass mod1 class1';
+        const mods = { mod1: true, mod2: false };
+        expect(classNames('someClass', mods, ['class1']))
+            .toBe(expected);
+    });
+});
