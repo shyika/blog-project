17da80897b0598b39c5280ce128e5be8e3eb6e54
Taras Shyika <shyika_taras@ukr.net>
2024-10-13 02:59:30 -0500

    feat: added Articles and ArticleDetails pages

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string
+}
+
+const ArticlesPage = (props: ArticlesPageProps) => {
+    const {
+        className,
+    } = props;
+    const { t } = useTranslation('articles');
+
+    return (
+        <div className={classNames(cls.articlesPage, {}, [className])}>
+            ARTICLES PAGE
+        </div>
+    );
+};
+
+export default memo(ArticlesPage);
