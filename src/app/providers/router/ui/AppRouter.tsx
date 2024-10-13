2bc150f698b7218f0b6b1deb682355d92f3d297c
Taras Shyika <shyika_taras@ukr.net>
2024-10-13 02:19:37 -0500

    fix: react router private routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,30 +1,27 @@
-import { memo, Suspense, useMemo } from 'react';
+import {
+    memo, Suspense, useCallback,
+} from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { AppRouteProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader/PageLoader';
-import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
+import { RequireAuth } from 'app/providers/router/ui/RequireAuth';
 
 const AppRouter = () => {
-    const isAuth = useSelector(getUserAuthData);
-
-    const routes = useMemo(() => {
-        return Object.values(routeConfig).filter((route) => {
-            if (route.authOnly && !isAuth) {
-                return false;
-            }
-
-            return true;
-        });
-    }, [isAuth]);
+    const renderWithWrapper = useCallback((route: AppRouteProps) => {
+        return (
+            <Route
+                key={route.path}
+                path={route.path}
+                element={route.authOnly ? <RequireAuth>{route.element}</RequireAuth> : route.element}
+            />
+        );
+    }, []);
 
     return (
         <Suspense fallback={<PageLoader />}>
             <div className="page-wrapper">
                 <Routes>
-                    {routes.map(({ path, element }) => (
-                        <Route path={path} element={element} key={path} />
-                    ))}
+                    {Object.values(routeConfig).map(renderWithWrapper)}
                 </Routes>
             </div>
         </Suspense>
